56e6ebd4321bd17fc3128e425e473d3e5bf65dc1
gkswogml23 <hindsight@hanjaehuiui-Macmini.local>
2021-03-11 02:24:53 +0900

    Test Encrypt Controller 암호, 복호화 테스트를 위한 컨트롤러 추가, 시연 후 삭제 예정

--- ConfigServer/src/main/java/com/ssafy/confsvr/config/JasyptConfig.java
+++ ConfigServer/src/main/java/com/ssafy/confsvr/config/JasyptConfig.java
@@ -15,13 +15,13 @@ public class JasyptConfig {
     public StringEncryptor stringEncryptor(Environment environment){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(environment.getProperty("jasypt.encryptor.password"));
+        config.setPassword(environment.getProperty("jasypt.encryptor.password")); // 패스워드 설정
         config.setAlgorithm("PBEWithSHA1ANdDESede"); // 암호화알고리즘 선택
-        config.setKeyObtentionIterations("1000"); // 아직 모름
-        config.setPoolSize("1"); // 아직 모름
-        config.setProvider(new BouncyCastleProvider());
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000"); // 해싱 반복 횟수
+        config.setPoolSize("1"); // 암호화 풀 사이즈 설정
+        config.setProvider(new BouncyCastleProvider()); // 암호화 알고리즘 provider 설정
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator"); // 솔트 생성기 지정
+        config.setStringOutputType("base64"); // 출력 타입 지
         encryptor.setConfig(config);
         return encryptor;
     }
