162c1ae546028a603f6564e5af9f072317fc1030
gkswogml23 <hindsight@hanjaehuiui-Macmini.local>
2021-03-10 23:06:33 +0900

    Feat Spring Cloud Config Server Config Server 개시

--- ConfigServer/src/main/java/com/ssafy/confsvr/ConfsvrApplication.java
+++ ConfigServer/src/main/java/com/ssafy/confsvr/ConfsvrApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.confsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // 컨피그 서버 설정
 public class ConfsvrApplication {
 
     public static void main(String[] args) {
