fde882e9742f9f6b74bac8603d3646e791a50893
gkswogml23 <hindsight@hanjaehuiui-Macmini.local>
2021-03-10 23:20:42 +0900

    Feat Spring Cloud Config Encrypt Config Server에 암호화 추가 Config 목록 중 디비 암호 등 암호화 처리해놓을 수 있음

---
+++ ConfigServer/src/main/java/com/ssafy/confsvr/config/JasyptConfig.java
@@ -0,0 +1,29 @@
+package com.ssafy.confsvr.config;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(Environment environment){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(environment.getProperty("jasypt.encryptor.password"));
+        config.setAlgorithm("PBEWithSHA1ANdDESede"); // 암호화알고리즘 선택
+        config.setKeyObtentionIterations("1000"); // 아직 모름
+        config.setPoolSize("1"); // 아직 모름
+        config.setProvider(new BouncyCastleProvider());
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
+
