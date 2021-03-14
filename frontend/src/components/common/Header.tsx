8c9173e07a24f37262af2e527807661a5fc16dd4
오수완 <ohsuwan1@gmail.com>
2021-03-15 00:11:49 +0900

    feat: material-ui example

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -23,24 +23,18 @@ const Wrapper = styled.div`
   }
 `;
 
-const LoginButton = styled.button`
-  font-size: 14px;
-  background-color: inherit;
-  color: #c2c2c2;
-  border: none;
-  &:hover {
-    transition: all 0.1s ease-in-out;
-    color: #f2f2f2;
-  }
-`;
-
 const Header = () => {
   return (
     <Wrapper>
-      <Link to="/">LOGO</Link>
-      <Link to="/login">
-        <LoginButton>로그인을 통해 딱 맞는 분석결과를 확인하세요</LoginButton>
-      </Link>
+      <div>
+        <Link to="/">LOGO</Link>
+        <Link to="/">일일 컨텐츠</Link>
+        <Link to="/">내 스크랩</Link>
+      </div>
+      <div>
+        <Link to="/signup">회원가입</Link>
+        <Link to="/login">로그인</Link>
+      </div>
     </Wrapper>
   );
 };
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { createInstance } from 'api/index';
 import axios, { AxiosInstance } from 'axios';
+import styled from 'styled-components';
+import {
+  Avatar,
+  Badge,
+  Button,
+  Card,
+  FormHelperText,
+  FormControl,
+  InputLabel,
+  Input,
+  Drawer,
+  FormGroup,
+} from '@material-ui/core';
 
 const Signup = () => {
+  // styles
+  const Wrapper = styled.main`
+    width: 100%;
+    height: 100vh;
+  `;
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const [url, setUrl] = useState('');
   const getCat = () => {
     return new Promise(async (resolve, reject) => {
@@ -16,7 +35,37 @@ const Signup = () => {
     });
   };
   getCat();
-  return <div>Signup</div>;
+  return (
+    <Wrapper>
+      <Avatar></Avatar>
+      <Badge>이것은 뱃지에요. 근데 뭔지 모르겠어요..</Badge>
+
+      {/* Drawer */}
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+        style={{ transition: 'all 0.5s ease-in-out' }}
+      >
+        이 공간 밖을 눌러보세요.
+      </Drawer>
+      {/* Drawer 버튼 */}
+      <Button onClick={() => setDrawerOpen(true)}>이 버튼을 눌러보세요.</Button>
+
+      {/* form-group 예제 with Card */}
+      <Card style={{ padding: '16px' }}>
+        <FormGroup>
+          <FormControl>
+            <InputLabel htmlFor="my-input">이메일</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <FormHelperText id="my-helper-text">
+              이메일을 입력해주세요.
+            </FormHelperText>
+          </FormControl>
+        </FormGroup>
+      </Card>
+    </Wrapper>
+  );
 };
 
 export default Signup;
