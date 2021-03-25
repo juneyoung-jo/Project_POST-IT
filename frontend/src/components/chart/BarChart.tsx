302d0237fe0b31e625bb12ae762540ef0a1961c6
오수완 <ohsuwan1@gmail.com>
2021-03-25 13:55:21 +0900

    chore: new branch for test

--- frontend/src/components/chart/BarChart.tsx
+++ frontend/src/components/chart/BarChart.tsx
@@ -12,7 +12,9 @@ function BarChart() {
   useLayoutEffect(() => {
     // create chart
     let chart = am4core.create('bar-chart', am4charts.XYChart);
-    chart.padding(40, 40, 40, 40);
+    chart.padding(40, 40, 40, 40); // 상, 하, 좌, 우
+
+    // Y축
     let categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
     categoryAxis.renderer.grid.template.location = 0;
     categoryAxis.dataFields.category = 'network';
@@ -20,9 +22,11 @@ function BarChart() {
     categoryAxis.renderer.inversed = true;
     categoryAxis.renderer.grid.template.disabled = true;
 
+    // X축
     let valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
     valueAxis.min = 0;
 
+    // 데이터 연결(차트 유형 별로 관련 시리즈가 따로 존재함)
     let series = chart.series.push(new am4charts.ColumnSeries());
     series.dataFields.categoryY = 'network';
     series.dataFields.valueX = 'MAU';
@@ -31,13 +35,15 @@ function BarChart() {
     series.columns.template.column.cornerRadiusBottomRight = 5;
     series.columns.template.column.cornerRadiusTopRight = 5;
 
+    // 내부 label
     let labelBullet = series.bullets.push(new am4charts.LabelBullet());
     labelBullet.label.horizontalCenter = 'left';
-    labelBullet.label.dx = 10;
+    labelBullet.label.dx = 10; // 라벨 text의 margin
     labelBullet.label.text =
-      "{values.valueX.workingValue.formatNumber('#.0as')}";
+      "{values.valueX.workingValue.formatNumber('#.0as')}"; // 소숫점 자리
     labelBullet.locationX = 1;
 
+    // 정렬
     categoryAxis.sortBySeries = series;
 
     chart.data = [
