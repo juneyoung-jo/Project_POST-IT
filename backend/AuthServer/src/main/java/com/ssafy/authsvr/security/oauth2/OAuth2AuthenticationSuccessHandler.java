478797984cf66961ef7856b3c8a1a85a772cdb5c
HanJaehee <hindsight@kakao.com>
2021-03-29 18:33:53 +0900

    Fix logging level root to info

--- backend/AuthServer/src/main/java/com/ssafy/authsvr/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ backend/AuthServer/src/main/java/com/ssafy/authsvr/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -81,6 +81,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .stream()
                 .anyMatch(authorizedRedirectUri -> {
                     // Only validate host and port. Let the clients use different paths if they want to
+                    System.out.println("uri : " + authorizedRedirectUri);
                     URI authorizedURI = URI.create(authorizedRedirectUri);
                     if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
                             && authorizedURI.getPort() == clientRedirectUri.getPort()) {
