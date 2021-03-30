d7149d0990f857e4616d843279a8f56baa04f185
김소정 <itoggi0328@naver.com>
2021-03-30 20:35:38 +0900

    Style: 메인페이지 재구성 - Full page 삭제 - Aos 라이브러리 적용 - Grid 적용

--- frontend/src/pages/MyFolder.tsx
+++ frontend/src/pages/MyFolder.tsx
@@ -1,12 +1,27 @@
 import styled from 'styled-components';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 function MyFolder() {
   const Wrapper = styled.div`
     width: 100%;
     height: 100vh;
   `;
-
-  return <Wrapper color="red"></Wrapper>;
+  const Test = styled.div`
+    height: 500px;
+    width: 300px;
+    background-color: #fff;
+    margin-bottom: 50px;
+  `;
+  return (
+    <Wrapper color="red">
+      <Test data-aos="fade-in"></Test>
+      <Test data-aos="fade-in"></Test>
+      <Test data-aos="fade-in"></Test>
+      <Test data-aos="fade-in"></Test>
+    </Wrapper>
+  );
 }
 
 export default MyFolder;
