ed53830fdfb62cec3014bcbe6ea6308d792b32bc
한재희 <gkswogml23@naver.com>
2021-04-01 01:08:58 +0000

    Update config.tsx

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -4,6 +4,7 @@ export const ACCESS_TOKEN = 'accessToken';
 
 export const OAUTH2_REDIRECT_URI = 'http://j4c103.p.ssafy.io:8443/oauth2/redirect';
 
+
 export const GOOGLE_AUTH_URL =
   API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 
