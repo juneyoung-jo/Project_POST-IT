66193c37067db9a117844531d65b22ea00931d30
이승준 <nate199458@gmail.com>
2021-04-01 12:16:09 +0900

    merge

--- backend/ReportServer/src/main/java/com/ssafy/entity/Blog.java
+++ backend/ReportServer/src/main/java/com/ssafy/entity/Blog.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Blog extends Contents{
 
     private int blogId;
-    private String img;
+    private String image;
 
 }
 
