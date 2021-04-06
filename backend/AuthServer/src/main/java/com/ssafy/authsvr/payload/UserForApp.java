3a9f8a94b52b2f448d3d9b76ad43f86fdfff4cef
HanJaehee <hindsight@kakao.com>
2021-04-07 01:51:42 +0900

    Fix: User Info Contents List type Integer to String

--- backend/AuthServer/src/main/java/com/ssafy/authsvr/payload/UserForApp.java
+++ backend/AuthServer/src/main/java/com/ssafy/authsvr/payload/UserForApp.java
@@ -14,8 +14,8 @@ public class UserForApp {
     private String name;
     private String email;
     private String imageUrl;
-    private List<Integer> categoryList;
-    private List<Integer> blogList;
-    private List<Integer> youtubeList;
-    private List<Integer> jobList;
+    private List<String> categoryList;
+    private List<String> blogList;
+    private List<String> youtubeList;
+    private List<String> jobList;
 }
