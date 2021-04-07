fb4c07bcfcbe8131e2dae571b0c56dbd3545d824
HanJaehee <hindsight@kakao.com>
2021-04-07 11:07:52 +0900

    Test: WebMvcConfig setting cors setting Specify allowed origins and allow withCredentials

--- backend/AuthServer/src/main/java/com/ssafy/authsvr/config/WebMvcConfig.java
+++ backend/AuthServer/src/main/java/com/ssafy/authsvr/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
+        .allowedOrigins("http://j4c103.p.ssafy.io", "http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
