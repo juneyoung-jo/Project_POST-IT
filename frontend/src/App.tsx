a67aa38c1a4735c2299712fcf52b0264bfbd0d52
김소정 <itoggi0328@naver.com>
2021-04-07 19:35:43 +0900

    Fix: 프로필 페이지 수정 - 유저 이름, 이메일, 사진 연결 - profile box padding 조정 - image 크기 조정 - 프로필 타이틀 margin 조정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,6 +35,9 @@ const App: React.FC = (): ReactElement => {
   const [authenticated, setAuthenticated] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
   const [loading, setLoading] = useState(false);
+  // const [name, setName] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [img, setImg] = useState('');
 
   useEffect(() => {
     function loadCurrentlyLoggedInUser() {
@@ -43,6 +46,7 @@ const App: React.FC = (): ReactElement => {
       getCurrentUser()
         .then((response) => {
           setCurrentUser(response), setAuthenticated(true), setLoading(false);
+          // setName(response.data.name), setEmail(response.data.email), setImg(response.data.imageUrl);
           console.log(response);
         })
         .catch((error) => {
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,8 +1,9 @@
 import { Container, Grid } from '@material-ui/core';
 import TopButton from 'components/common/TopButton';
 import { Link } from 'react-router-dom';
-import React from 'react';
 import styled from 'styled-components';
+import React, { ReactElement, Suspense, useState, useEffect } from 'react';
+import { getCurrentUser } from 'api/user';
 
 const customMediaQuery = (maxWidth: number) =>
   `@media (max-width: ${maxWidth}px)`;
@@ -21,7 +22,8 @@ const ProfileBox = styled.div`
   padding: 100px;
 
   ${media.sm} {
-    padding: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
   }
   display: flex;
   flex-direction: column;
@@ -58,11 +60,12 @@ const Title = styled.div`
   text-align: center;
   line-height: 1.3;
   word-break: keep-all;
+  margin-bottom: 50px;
 `;
 
-const Profileimg = styled.div`
-  width: 300px;
-  height: 300px;
+const Profileimg = styled.img`
+  width: 250px;
+  height: 250px;
   background-color: ${({ theme }) => theme.colors.first};
   border-radius: 200px;
   margin-top: 30px;
@@ -107,14 +110,31 @@ const Button = styled.button`
 `;
 
 const Profile = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [img, setImg] = useState('');
+
+  useEffect(() => {
+    getCurrentUser()
+      .then((response) => {
+        setName(response.data.name),
+          setEmail(response.data.email),
+          setImg(response.data.imageUrl);
+        // console.log(response);
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
+  }, []);
+
   return (
     <Container>
       <TopButton></TopButton>
       <ProfileBox>
-        <Title>사용자님의 프로필</Title>
-        <Profileimg></Profileimg>
-        <div className="user-info">사용자</div>
-        <div className="user-info">user@user.com</div>
+        <Title>{name}님의 프로필</Title>
+        <Profileimg src={img}></Profileimg>
+        <div className="user-info">{name}</div>
+        <div className="user-info">{email}</div>
       </ProfileBox>
     </Container>
   );
